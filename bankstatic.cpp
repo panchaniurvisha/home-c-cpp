bc42917a619f33edc2a7b6ffebf60ced6e508cba
urvisha <urvisha1109@gmail.com>
2022-10-10 21:45:18 +0530

    first commit

--- bankstatic.cpp
+++ bankstatic.cpp
@@ -1,59 +1,50 @@
 #include<iostream>
 using namespace std;
-class Bank{
-    
+class Bank{ 
     string name;
-    int accbalance,wdbalance,dpbalance;
+    int accbalance;
     public:
-    static int totaltracount;
-    static int totaldptracount;
-    static int totalwdtracount;
-void getdata(){
-    cout<<"\n person Name :";
-    cin>>name;
-}
+    static int totaltracount,totaldpcount,totalwdcount,totaltraAmoucount,totaldpAmocount,totalwdAmocount;
 Bank(){
     accbalance=1000;
 }
 Bank(int initbalance){
     accbalance=initbalance;
 }
-void showAccBalance(){
+void getdata(){
+    cout<<"\n person Name :";
+    cin>>name;
+}
+void WdBalance(int amt){
     cout<<"\n enter account balance: ";
     cin>>accbalance;
     cout<<"\n enter withdraw balance :";
-    cin>>wdbalance;
-    cout<<"\n reminder balance:"<<accbalance-wdbalance;
+    cin>>amt;
+    totalwdAmocount+=amt;
+}
+void DpBalance(int amt){
     cout<<"\n\n enter deposite balance :";
-    cin>>dpbalance;
-    cout<<"\n total balance :"<<(accbalance-wdbalance)+dpbalance;
+    cin>>amt;
+    totaldpAmocount+=amt;
 }
 void setcount(int x,int y,int z){
     totaltracount=x;
-    totaldptracount=y;
-    totalwdtracount=z;
+    totaldpcount=y;
+    totalwdcount=z;
  }
 void Counter(){
     totaltracount++;
-    totaldptracount++;
-    totalwdtracount++;
-}
-/*void GetCount(){
-    cout<<"\n "<<totaltracount;
-    cout<<"\n"<<totaldptracount;
-    cout<<"\n "<< totalwdtracount;
-}*/
-void showAccDetails(){
-    cout<<"\n enter account balance: "<<accbalance;
-    cout<<"\n enter withdraw balance :"<<wdbalance;
-    cout<<"\n reminder balance:"<<accbalance-wdbalance;
-    cout<<"\n enter deposite balance :"<<dpbalance;
-    cout<<"\n total balance :"<<(accbalance-wdbalance)+dpbalance;
+    totaldpcount++;
+    totalwdcount++;
+    totaltraAmoucount=totaldpAmocount+totalwdAmocount;
 }
 };
 int Bank::totaltracount=0;
-int Bank::totaldptracount=0;
-int Bank::totalwdtracount=0;
+int Bank::totaldpcount=0;
+int Bank::totalwdcount=0;
+int Bank::totaltraAmoucount=0;
+int Bank::totaldpAmocount=0;
+int Bank::totalwdAmocount=0;
 int main()
 {
     Bank p[5];
@@ -61,14 +52,15 @@ int main()
     for(i=1;i<=3;i++)
     {
         p[i].getdata();
-        p[i].showAccBalance();
-        p[i].showAccDetails();
+        p[i].WdBalance(5000);
+        p[i].DpBalance(10000);
+        p[i].setcount(5,2,2);
+        p[i].Counter();
     }
-    Bank person1,person2,person3;
-    person1.setcount(5,2,2);
-    person3.Counter();
-    //person1.GetCount();
-        cout<<"\n total Transaction Count :"<<Bank::totaltracount;
-        cout<<"\n total Deposite Transaction Count :"<<Bank::totaldptracount;
-        cout<<"\n total Withdraw Transaction Count :"<<Bank::totalwdtracount;
+    cout<<"\n total Transaction Count :"<<Bank::totaltracount;
+    cout<<"\n total Deposite Transaction Count :"<<Bank::totaldpcount;
+    cout<<"\n total Withdraw Transaction Count :"<<Bank::totalwdcount;
+    cout<<"\n Total transation Amount :"<<Bank::totaltraAmoucount;
+    cout<<"\n Total transation Deposite Amount :"<<Bank::totaldpAmocount;
+    cout<<"\n Total transation Withdraw Amount :"<<Bank::totalwdAmocount;
 }
