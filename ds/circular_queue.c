4e2abf54224237599beeea5c7c0d45b14568b6aa
urvisha <urvisha1109@gmail.com>
2022-11-30 19:37:11 +0530

    first commit

--- ds/circular_queue.c
+++ ds/circular_queue.c
@@ -1,95 +1,95 @@
-#include<stdio.h>
-#define size 5
-int queue[size];
-int front = -1,rear = -1;
+#include <stdio.h>
+#define Size 5
+int queue[Size];
+int front = -1 ;
+int rear = -1;
 void insert_queue(int added_item)
 {
-    if(front==-1 && rear == -1)
+    if (front == -1&& rear == -1)
     {
-        front=0;
-        rear=0;
-        queue[rear]=added_item;
+        front = 0;
+        rear = 0;
+        queue[rear] = added_item;
     }
-    else if((rear+1)%size==front)
+    else if ((rear + 1) % Size == front)
     {
-        printf("queue is overflow");
+        printf("Queue is overflow..");
     }
     else
     {
-        rear=(rear+1)%size;
-        queue[rear]=added_item;// assigning a value to the queue at the rear position.
+        rear = (rear + 1) % Size;
+        queue[rear] = added_item;
     }
 }
 int delete_queue()
 {
-    if((front==-1)&&(rear==-1))
+    if ((front == -1) && (rear == -1))
     {
-        printf("\n queue is underflow");
+        printf("\nQueue is underflow..");
     }
-    else if(front==rear)
+    else if (front == rear)
     {
-        printf("\n deleted elements is :%d",queue[front]);
-        front=-1;
-        rear=-1;
+        printf("\nThe Deleted element is %d", queue[front]);
+        front = -1;
+        rear = -1;
     }
     else
     {
-        printf("\n the deleted elements is :%d",queue[front]);
-        front=(front+1)%size;
+        printf("\nThe Deleted element is %d", queue[front]);
+        front = (front + 1) % Size;
     }
 }
 void display_queue()
 {
-    if(front=-1)
+    if (front == -1)
     {
-        printf("\n queue is empty");
+        printf("\nQueue is Empty");
         return;
     }
-    printf("\n queue elements :");
-    if(rear>=front)
+    printf("\nQueue elements : ");
+    if (rear >= front)
     {
-        for(int i=front;i<=rear;i++)
-            printf("%d",queue[i]);
+        for (int i = front; i <= rear; i++)
+            printf("%d ", queue[i]);
     }
     else
     {
-        for(int i=front;i<size;i++)
-        printf("%d",queue[i]);
-        
-        for(int i=0;i<=rear;i++)
-        printf("%d",queue[i]);
+        for (int i = front; i < Size; i++)
+            printf("%d ", queue[i]);
+
+        for (int i = 0; i <= rear; i++)
+            printf("%d ", queue[i]);
     }
 }
-int main()  
-{  
-    int choice,x;   // variables declaration  
-      
-    while(choice!=0)   // while loop  
-    {  
-        printf("\nPress 1: Insert an element");  
-        printf("\nPress 2: Delete an element");  
-        printf("\nPress 3: Display the element");  
-        printf("\nEnter your choice :");  
-        scanf("%d", &choice);  
-      
-        switch(choice)  
-        {  
-            case 1:  
-            printf("Enter the element which is to be inserted :");  
-            scanf("%d",&x);  
-            insert_queue(x);  
-            break; 
+int main()
+{
+    int choice, x;
+    do
+    {
+        printf("\n1.Insert \n");
+        printf("2.Delete \n");
+        printf("9.Display\n");
+        printf("0.Quit\n");
+        printf("Enter your choice : ");
+        scanf("%d", &choice);
 
-            case 2:  
-            delete_queue();  
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the element which is to be inserted");
+            scanf("%d", &x);
+            insert_queue(x);
             break;
-
-            case 3:  
-            display_queue(); 
-            break; 
+        case 2:
+            delete_queue();
+            break;
+        case 9:
+            display_queue();
+            break;
+        case 0:
+            break;
+        default:
+            printf("Wrong choice\n");
         }
-    }
-    return 0;  
-}  
-
-
+    } while (choice != 0);
+}
---
+++ ds/fibonacci_recursion.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int fibonacci(int);
+int main()
+{
+    int n,i;
+    printf("enter number :");
+    scanf("%d",&n);
+    printf("fibonacci series :");
+    for(i=0;i<=n;i++)
+    {
+        printf("%d\t",fibonacci(i));
+    }
+}
+int fibonacci(int i)
+{
+    if(i==0)
+    {
+        return 0;
+    }
+    else if(i==1)
+    {
+        return 1;
+    }
+    else
+    return (fibonacci(i-1) + fibonacci(i-2));
+}
