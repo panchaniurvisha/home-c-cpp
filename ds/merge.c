2571aad859dcdbc5aa96a59dcfc66cc9e15208d6
urvisha <urvisha1109@gmail.com>
2022-12-01 18:09:46 +0530

    first commit

--- ds/merge.c
+++ ds/merge.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
+#define size 5
 int main()
 {
-    int a[50],b[50],n,c[20],i,k;
-    printf("\n enter array size:");
-    scanf("%d",&n);
+    int a[size],b[size];
     printf("\n enter array elements :");
-    for(int i=0;i<n;i++){
-        scanf("\t%d",&a[i]);
-    }
+    for(int i=0;i<size;i++)
+        scanf("%d",&a[i]);
     printf("\n enter array elements :");
-    for(i=0;i<n;i++){
-        scanf("\t%d",&b[i]);
-    }
-    for(int i=0;i<n;i++)
+    for(int i=0;i<size;i++)
+        scanf("%d",&b[i]);
+    for(int i=0;i<size;i++)
        printf("%d\n",a[i]);
     return 0;
 }
