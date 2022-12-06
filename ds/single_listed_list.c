5f91ef6b1254b44ec5249575102f2fe7b23678e6
urvisha <urvisha1109@gmail.com>
2022-12-07 00:01:11 +0530

    first commit

--- ds/single_listed_list.c
+++ ds/single_listed_list.c
@@ -4,7 +4,7 @@
 struct Node
 {
     int data;
-    struct node *next;
+    struct Node *next;
 };
 void linkedListTraversal(struct Node *head)
 {
@@ -20,6 +20,55 @@ void linkedListTraversal(struct Node *head)
         ptr=ptr->next;
     }
     printf("\n list count :%d",count);
+}
+//Case 1:Deleting the first element from linked list
+struct Node *deleteFirst(struct Node *head){   
+    struct Node *ptr=head;
+    head = head ->next;
+    free(ptr);
+    return head;
+}
+//Case 2:Deleting the element at given index from linked list
+struct Node *deleteAtIndex(struct Node *head,int index){
+    struct Node *p=head;
+    struct Node *q=head->next;
+    for(int i=0;i< index -1;i++)
+    {
+        p=p->next;
+        q=q->next;
+    }
+    p->next=q->next;
+    free(q);
+    return head;
+}
+//case 3:Deleting the last elements
+struct Node *deleteAtLast(struct Node *head){
+    struct Node *p=head;
+    struct Node *q=head->next;
+    while(q->next !=NULL)
+    {
+        p=p->next;
+        q=q->next;
+    }
+    p->next =NULL;
+    free(q);
+    return head;
+}
+//case4:Deleting the elements with a given value from the linked list
+struct Node *deleteAtIndex(struct Node *head,int value){
+    struct Node *p=head;
+    struct Node *q=head->next;
+    while(q->data!=value && q->next!=NULL)
+    {
+        p=p->next;
+        q=q->next;
+    }
+    if(q->data == value)
+    {
+        p->next = q->next;
+        free(q);
+    }
+    return head;
 }
 int main()
 {
@@ -47,6 +96,13 @@ int main()
     fourth->data=31;
     fourth->next=NULL;
 
+    printf("Linked list before deletion\n");
+    linkedListTraversal(head);
+    head= deleteFirst(head);//for deleting first element of the linked list
+    head = deleteAtIndex(head,2);//
+    head = deleteAtLast(head);
+
+    printf("Linked list after  deletion \n");
     linkedListTraversal(head);
     return 0;
 }
