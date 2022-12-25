71a64353178f57b97969d3792e6aafac9b2e552e
urvisha <urvisha1109@gmail.com>
2022-12-25 11:13:50 +0530

    first commit

--- dart/variable.dart
+++ dart/variable.dart
@@ -1,5 +1,5 @@
 void main(){
-   /*int data = 25,number =100;
+   int data = 25,number =100;
    double cm =12.50,height =5.4;
    String str1 = 'this is a single line string',str2 = "I Am Urvisha",str3 = "hello",str4 = "world",str5 = "0+10",res= "str3 + str4";
    bool value1 =100<20,value2 =100>20;
@@ -14,19 +14,5 @@ void main(){
    print(res);
    print(str5);
    print(value1);
-   print(value2);*/
-   //dynamic and var data type
-   dynamic data = "abc"; //initially it's a string
-            print(data);
-            data = 123; //then we assign an int value to it
-            print(data);
-            data = true; //and then a bool
-            print(data);
-    var name ="urvish zalavadiya";
-            print(name);
-            //name=100;
-            //print(name);
-            name="skill quode";
-            print(name);
-  
-}
+   print(value2);
+}  
