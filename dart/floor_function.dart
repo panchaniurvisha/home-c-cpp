41d687f7f7d673f15027e4c7c0dda4b80b00d6b7
urvisha <urvisha1109@gmail.com>
2022-12-31 22:40:49 +0530

    first commit

---
+++ dart/floor_function.dart
@@ -0,0 +1,10 @@
+void main() {
+    //positive number 
+    print("The value of (1.5).floor(): ${(1.5).floor()} ");
+
+    // negative number 
+    print("The value of (-5.5).floor(): ${(-5.5).floor()} ");
+
+    //zero
+    print("The value of (0).floor(): ${(0).floor()}");
+}
---
+++ dart_practise-18programm/17.count_specific_letter.dart
@@ -0,0 +1,7 @@
+void countLetter(String str,String str1){
+  print("Enter String Letter :h");
+  print('h'.allMatches('urvisha').length);
+}
+void main() {
+  countLetter("urvisha",'h');
+}
