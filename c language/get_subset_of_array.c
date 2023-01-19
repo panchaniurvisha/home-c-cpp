f4e6aaab0d46b85895353ae1f5bd3c9540e964fa
urvisha <urvisha1109@gmail.com>
2023-01-19 16:39:12 +0530

    first commit

--- c language/get_subset_of_array.c
+++ c language/get_subset_of_array.c
@@ -3,28 +3,17 @@ void printCombination(int arr[], int n, int r){
     int data[r];
     combinationUtil(arr, n, r, 0, data, 0);
 }
-   /*index  ---> Current index in data[]
-   data[] ---> Temporary array to store current combination
-   i      ---> index of current element in arr[]     */
 void combinationUtil(int arr[], int n, int r, int index,int data[], int i){
-    // Current combination is ready, print it
     if (index == r) {
         for (int j = 0; j < r; j++)
             printf("%d ", data[j]);
         printf("\n");
         return;
     }
-    // When no more elements are there to put in data[]
     if (i >= n)
         return;
- 
-    // current is included, put next at next location
     data[index] = arr[i];
     combinationUtil(arr, n, r, index + 1, data, i + 1);
- 
-    // current is excluded, replace it with next
-    // (Note that i+1 is passed, but index is not
-    // changed)
     combinationUtil(arr, n, r, index, data, i + 1);
 }
 int main(){
--- dart_practise-20programm/18.get_subsets.dart
+++ dart_practise-20programm/18.get_subsets.dart
@@ -1,45 +1,14 @@
-/*List getAllSubsets(l)=> l.fold<dynamic>([[ ]],(subLists,element){
-    return subLists.map((subList) =>[subList,subList+[element]]).expand((element)=>element).toList();
-  });
-void main(){
-  ///String str="hello world";
-  List<int> l=[1,2,3];
-  print(getAllSubsets(l));
-}*/
-import 'dart:io';
-void subset(List arr[], int data[], int start, int end, int index, int r)
-{
-    int j, i;
-    if (index == r) {
-        for (j = 0; j < r; j++)
-            print(%$data[j]);
-        print("\n");
-        return;
+void main() {
+  List result = [];
+  List data = [1, 2, 3];
+  //int fixed length=2;
+  for (int i = 0; i < data.length; i++) {
+    for (int j = i; j < data.length; j++) {
+      if (i != j) {
+        List temp = [data[i], data[j]];
+        result.add(temp);
+      }
     }
-    for (i = start; i <= end && end - i + 1 >= r - index; i++)
-    {
-        data[index] = arr[i];
-        subset(arr, data, i+1, end, index+1, r);
-    }
-}
-void printsubset(List arr, int n, int r)
-{
-    List data=[r];
-    subset(arr, data, 0, n - 1, 0, r);
-}
-int main()
-{
-    int ?k, n;
-    List arr=[1,2,3];
-    print("Enter the number of input : ");
-    n=int.parse(stdin.readLineSync()!);//scanf("%d", &n);
-    /*print("\nEnter the integers: \n");
-    for (  i = 0; i !< n; i++)
-    {
-	    int.parse(stdin.readLineSync()!);//scanf("%d", &arr[i]);
-    }*/
-    print("Enter value of k: ");
-    k=int.parse(stdin.readLineSync()!);
-    printsubset(arr, n, k);
-    return 0;
+  }
+  print(result);
 }
